c5fb3b338b223b6d1782766ef4d0e4c5f872f92c
jzg <aha_isgg@outlook.com>
2024-06-30 15:23:31 +0800

    temp

---
+++ eval/platform.h
@@ -0,0 +1,8 @@
+//
+// Created by kim on 2024/6/30.
+//
+
+#ifndef C6NNUE_PLATFORM_H
+#define C6NNUE_PLATFORM_H
+
+#endif  // C6NNUE_PLATFORM_H
---
+++ eval/utils.h
@@ -0,0 +1,8 @@
+//
+// Created by kim on 2024/6/30.
+//
+
+#ifndef C6NNUE_UTILS_H
+#define C6NNUE_UTILS_H
+
+#endif  // C6NNUE_UTILS_H
